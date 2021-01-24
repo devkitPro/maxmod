a97caf8db30b3cc8e9de591e305daa8a5c1f070f
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-01-24 00:33:36 +0000

    Rewrite mm_main.s in C
    Some ASM helpers have been taken out into a new file as they will be
    needed for a while until most of the code is C.

---
+++ source/mm_main.c
@@ -0,0 +1,36 @@
+/****************************************************************************
+ *                                                          __              *
+ *                ____ ___  ____ __  ______ ___  ____  ____/ /              *
+ *               / __ `__ \/ __ `/ |/ / __ `__ \/ __ \/ __  /               *
+ *              / / / / / / /_/ />  </ / / / / / /_/ / /_/ /                *
+ *             /_/ /_/ /_/\__,_/_/|_/_/ /_/ /_/\____/\__,_/                 *
+ *                                                                          *
+ *      Copyright (c) 2021, Antonio Niño Díaz (antonio_nd@outlook.com)      *
+ *         Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)         *
+ *                                                                          *
+ * Permission to use, copy, modify, and/or distribute this software for any *
+ * purpose with or without fee is hereby granted, provided that the above   *
+ * copyright notice and this permission notice appear in all copies.        *
+ *                                                                          *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES *
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         *
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  *
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   *
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    *
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  *
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           *
+ ****************************************************************************/
+
+#include "mm_types.h"
+
+// Function pointer to user event handler
+mm_callback mmCallback;
+
+// Bitmask to select which hardware/software channels can be used
+mm_word mm_ch_mask;
+
+// Set function for handling playback events
+void mmSetEventHandler(mm_callback handler)
+{
+    mmCallback = handler;
+}
