3ba28a9ba9d0c7f40af4419eb8eeb30351a55f93
fincs <fincs@devkitpro.org>
2023-02-25 17:48:13 +0100

    Begin adding calico arm7 threading/pxi code

---
+++ source_calico/mm_comms7.c
@@ -0,0 +1,182 @@
+#include <calico.h>
+#include <maxmod7.h>
+#include "mm_pxi.h"
+
+#define MM_NUM_MAIL_SLOTS 2
+#define MM_MAIL_EXIT      0
+#define MM_MAIL_WAKEUP    1
+#define MM_MAIL_TIMER     2
+
+static Thread s_mmThread;
+alignas(8) static u8 s_mmThreadStack[512];
+
+static Mailbox s_mmMailbox, s_mmPxiMailbox;
+static u32 s_mmPxiMailboxSlots[MM_PXI_NUM_CREDITS];
+
+extern u8 mmInitialized;
+
+MEOW_CODE32 MEOW_NOINLINE static void _mmProcessComms(void);
+
+// Missing prototypes for "internal" maxmod functions
+void mmMixerPre(void);
+void mmUpdateEffects(void);
+void mmPulse(void);
+void mmMixerMix(void);
+void mmGetSoundBank(u16 num_songs, void* mm_bank);
+
+MEOW_CODE32 static void _mmTimerIsr(void)
+{
+	// Do nothing if maxmod is not initialized
+	if (mmInitialized != 42) {
+		return;
+	}
+
+	// Disable timer IRQ
+	REG_TMxCNT_H(1) &= ~TIMER_ENABLE_IRQ;
+
+	// Call timing critical mixer handler
+	mmMixerPre();
+
+	// Send timer message to maxmod thread
+	mailboxTrySend(&s_mmMailbox, MM_MAIL_TIMER);
+}
+
+static void _mmPxiHandler(void* user, u32 data)
+{
+	// Add message to PXI mailbox
+	mailboxTrySend((Mailbox*)user, data);
+
+	// Wake up maxmod thread if needed
+	if (s_mmMailbox.pending_slots == 0) {
+		mailboxTrySend(&s_mmMailbox, MM_MAIL_WAKEUP);
+	}
+}
+
+static int _mmThreadMain(void* arg)
+{
+	// Set up timer IRQ
+	irqSet(IRQ_TIMER1, _mmTimerIsr);
+	irqEnable(IRQ_TIMER1);
+
+	// Set up PXI and mailboxes
+	u32 slots[MM_NUM_MAIL_SLOTS];
+	mailboxPrepare(&s_mmMailbox, slots, MM_NUM_MAIL_SLOTS);
+	mailboxPrepare(&s_mmPxiMailbox, s_mmPxiMailboxSlots, MM_PXI_NUM_CREDITS);
+	pxiSetHandler(PxiChannel_Maxmod, _mmPxiHandler, &s_mmPxiMailbox);
+
+	for (;;) {
+		// Get message
+		u32 msg = mailboxRecv(&s_mmMailbox);
+		if (msg == MM_MAIL_EXIT) {
+			break;
+		}
+
+		// Handle periodic timer updates
+		if (msg == MM_MAIL_TIMER) {
+			mmUpdateEffects();
+			mmPulse();
+			mmMixerMix();
+
+			// Reenable timer IRQ
+			REG_TMxCNT_H(1) |= TIMER_ENABLE_IRQ;
+		}
+
+		// Handle PXI commands
+		_mmProcessComms();
+	}
+
+	return 0;
+}
+
+void mmInstall(int thread_prio)
+{
+	threadPrepare(&s_mmThread, _mmThreadMain, NULL, &s_mmThreadStack[sizeof(s_mmThreadStack)], thread_prio);
+	threadStart(&s_mmThread);
+}
+
+MEOW_NOINLINE static void _mmProcessPxiCmd(mmPxiCmd cmd, unsigned imm, const void* body, unsigned num_words)
+{
+	switch (cmd) {
+		default: {
+			//dietPrint("[MM] Unhandled cmd %u\n", cmd);
+			break;
+		}
+
+		case mmPxiCmd_Bank: {
+			const mmPxiArgBank* arg = (const mmPxiArgBank*)body;
+			mmGetSoundBank(arg->num_songs, arg->mm_bank);
+			break;
+		}
+
+		case mmPxiCmd_SelChan: {
+			bool is_lock = (imm & (1U<<16)) != 0;
+			imm &= 0xffff;
+			if (is_lock) {
+				mmLockChannels(imm);
+			} else {
+				mmUnlockChannels(imm);
+			}
+			break;
+		}
+
+		case mmPxiCmd_Start: {
+			mmStart(*(const mm_word*)body, (mm_pmode)imm);
+			break;
+		}
+
+		case mmPxiCmd_Pause: {
+			mmPause();
+			break;
+		}
+
+		case mmPxiCmd_Resume: {
+			mmResume();
+			break;
+		}
+
+		case mmPxiCmd_Stop: {
+			mmStop();
+			break;
+		}
+
+		case mmPxiCmd_SelectMode: {
+			mmSelectMode((mm_mode_enum)imm);
+			break;
+		}
+	}
+}
+
+void _mmProcessComms(void)
+{
+	u32 cmdheader;
+	unsigned credits = 0;
+	while (mailboxTryRecv(&s_mmPxiMailbox, &cmdheader)) {
+		unsigned num_words = cmdheader >> 26;
+		if (num_words) {
+			cmdheader &= (1U<<26)-1;
+		}
+
+		mmPxiCmd cmd = mmPxiCmdGetType(cmdheader);
+		unsigned imm = mmPxiCmdGetImm(cmdheader);
+		credits += 1 + num_words;
+
+		if_likely (num_words == 0) {
+			// Process command directly
+			_mmProcessPxiCmd(cmd, imm, NULL, 0);
+		} else {
+			// Receive message body
+			u32 body[num_words];
+			for (unsigned i = 0; i < num_words; i ++) {
+				body[i] = mailboxRecv(&s_mmPxiMailbox);
+			}
+
+			// Process command
+			_mmProcessPxiCmd(cmd, imm, body, num_words);
+		}
+	}
+
+	if (credits) {
+		// Return credits to arm9
+		MEOW_DUMMY(credits);
+	}
+}
---
+++ source_calico/mm_pxi.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#ifdef SYS_CALICO
+#include <calico.h>
+#define MM_INLINE    MEOW_INLINE
+#define MM_CONSTEXPR MEOW_CONSTEXPR
+#define PxiChannel_Maxmod ((PxiChannel)22)
+#else
+#include <libnds.h>
+#define MM_INLINE    static inline
+#define MM_CONSTEXPR static inline
+#endif
+
+#define MM_PXI_NUM_CREDITS 64
+
+typedef enum mmPxiCmd {
+	mmPxiCmd_Bank            = 0,
+	mmPxiCmd_SelChan         = 1,
+	mmPxiCmd_Start           = 2,
+	mmPxiCmd_Pause           = 3,
+	mmPxiCmd_Resume          = 4,
+	mmPxiCmd_Stop            = 5,
+	mmPxiCmd_Position        = 6,
+	mmPxiCmd_StartSub        = 7,
+	mmPxiCmd_MasterVol       = 8,
+	mmPxiCmd_MasterVolSub    = 9,
+	mmPxiCmd_MasterTempo     = 10,
+	mmPxiCmd_MasterPitch     = 11,
+	mmPxiCmd_MasterEffectVol = 12,
+	mmPxiCmd_SelectMode      = 15,
+	mmPxiCmd_Effect          = 16,
+	mmPxiCmd_EffectVol       = 17,
+	mmPxiCmd_EffectPan       = 18,
+	mmPxiCmd_EffectRate      = 19,
+	mmPxiCmd_EffectMulRate   = 20,
+	mmPxiCmd_EffectOpt       = 21,
+	mmPxiCmd_EffectEx        = 22,
+	mmPxiCmd_EffectCancelAll = 29,
+} mmPxiCmd;
+
+typedef struct mmPxiArgBank {
+	u16 num_songs;
+	void* mm_bank;
+} mmPxiArgBank;
+
+MM_CONSTEXPR u32 mmPxiMakeCmdMsg(mmPxiCmd cmd, unsigned imm)
+{
+	return (cmd & 0x1f) | (imm << 5);
+}
+
+MM_CONSTEXPR mmPxiCmd mmPxiCmdGetType(u32 msg)
+{
+	return (mmPxiCmd)(msg & 0x1f);
+}
+
+MM_CONSTEXPR unsigned mmPxiCmdGetImm(u32 msg)
+{
+	return msg >> 5;
+}
